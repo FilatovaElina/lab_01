cfe09bf32e38e4becf62db94794482a3b747aba6
FilatovaElina <filatova.elia@gmail.com>
2024-09-29 21:47:15 +0300

    Добавьте файлы проекта.

---
+++ lab_01.cpp
@@ -0,0 +1,29 @@
+﻿// lab_01.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+int FindIndex(int arr[], int size, int n)
+{
+
+    for (int i = 0; i < size; i++) 
+        if (arr[i] == n) 
+            return i;     
+               
+    
+    return -1;
+}
+
+int main()
+{
+    int arr[]{ 3,141,592,6,33,58 };
+    int N;
+    std::cout << "Input N: ";
+    std::cin >> N;
+    int size = sizeof(arr) / sizeof (arr[0]);
+    int inx = FindIndex(arr, size, N);
+
+    std::cout << "inx=" << inx;
+    return 0;
+
+}
+
